39d6fef70cef5b2322439eabbb125953247bfb80
Andreas Blaabjerg <Andreas.blaabjerg1@hotmail.com>
2020-04-16 13:40:53 +0200

    Initial commit

---
+++ app/src/main/java/com/example/appproject_group25_itsmap_f20_master_disaster_broadcaster/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.appproject_group25_itsmap_f20_master_disaster_broadcaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
