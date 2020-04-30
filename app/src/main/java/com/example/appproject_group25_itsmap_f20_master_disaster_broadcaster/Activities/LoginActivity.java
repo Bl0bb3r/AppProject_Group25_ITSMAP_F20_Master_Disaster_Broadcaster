83d4633bfff99e7ad19467b13591f6dbcfde7458
Bl0bb3r <Andreas.blaabjerg1@hotmail.com>
2020-04-30 14:34:37 +0200

    Login and register account added temporary version

--- app/src/main/java/com/example/appproject_group25_itsmap_f20_master_disaster_broadcaster/Activities/LoginActivity.java
+++ app/src/main/java/com/example/appproject_group25_itsmap_f20_master_disaster_broadcaster/Activities/LoginActivity.java
@@ -1,22 +1,123 @@
 package com.example.appproject_group25_itsmap_f20_master_disaster_broadcaster.Activities;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import com.example.appproject_group25_itsmap_f20_master_disaster_broadcaster.R;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import static android.content.ContentValues.TAG;
+import static java.security.AccessController.getContext;
+
 public class LoginActivity extends AppCompatActivity {
 
+    Button ExitBtn;
+    Button SignInBtn;
+    TextInputEditText EmailField;
+    TextInputEditText PasswordField;
 
+    //Firebase authentication variable
+    public static FirebaseAuth mAuth;
+
+
+    //TODO: Insert NavController
+
+    @Override
+    public void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+
+        setContentView(R.layout.activity_login);
+
+        // Views
+        EmailField = findViewById(R.id.EmailEditText);
+        PasswordField = findViewById(R.id.PasswordEditText);
+
+        // Buttons
+        ExitBtn = findViewById(R.id.Btn_Exit);
+        SignInBtn = findViewById(R.id.Btn_SignIn);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        // Exit Button listener - Closes and exits application
+        ExitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                System.exit(0);
+            }
+        });
+
+
+        // Sign in Button listener
+        SignInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
 
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+
+    }
+
+    public void Authenticate(String Email, String Password, final View view) {
+        try {
+            mAuth.signInWithEmailAndPassword(Email, Password).addOnCompleteListener((Activity) LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        //Authenticate
+                        Toast.makeText(LoginActivity.this, "Authentication successfull.", Toast.LENGTH_LONG).show();
+                        getCurrentUser();
+                        //TODO: Look at Nav Controller
+                        //Init Navigation
+                        //final NavController navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment);
+
+                        //Hide Keyboard
+                        InputMethodManager imm = (InputMethodManager) LoginActivity.this.getSystemService(Context.INPUT_METHOD_SERVICE);
+                        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+                        //Navigate
+                        //navController.navigate(R.id.action_loginFragment_to_settingsOverviewFragment);
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Authentication failed. Try Again", Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+        } catch (Exception e) {
+            Snackbar.make(LoginActivity.this.findViewById(android.R.id.content),
+                    e.getLocalizedMessage(), Snackbar.LENGTH_LONG).show();
+        }
+    }
+
+    public static FirebaseUser getCurrentUser() {
+        FirebaseUser CurrentUser = mAuth.getCurrentUser();
+        return CurrentUser;
+    }
+
+
+}
---
+++ app/src/main/java/com/example/appproject_group25_itsmap_f20_master_disaster_broadcaster/Fragments/CreateNewUserFragment.java
@@ -0,0 +1,124 @@
+package com.example.appproject_group25_itsmap_f20_master_disaster_broadcaster.Fragments;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.appproject_group25_itsmap_f20_master_disaster_broadcaster.Activities.LoginActivity;
+import com.example.appproject_group25_itsmap_f20_master_disaster_broadcaster.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseUser;
+
+import static android.content.ContentValues.TAG;
+import static com.example.appproject_group25_itsmap_f20_master_disaster_broadcaster.Activities.LoginActivity.mAuth;
+
+public class CreateNewUserFragment extends Fragment {
+
+    private LoginActivity loginActivity;
+
+    Button BackBtn;
+    Button SignUpBtn;
+    TextInputEditText Email;
+    TextInputEditText Password;
+
+    //TODO: Firebase database med users?
+    //FirebaseDatabase FirebaseDB;
+
+    private View rootView;
+
+
+    public CreateNewUserFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        //get so that i can access DisasterService that is bound to main Activity.
+        loginActivity = (LoginActivity) context;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        rootView = inflater.inflate(R.layout.fragment_create_new_user, container, false);
+        return rootView;
+    }
+
+    // Firebase setup assistant
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // Firebase setup assistant
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser ExistingUser = LoginActivity.getCurrentUser();
+        updateUI(ExistingUser);
+    }
+
+    private void updateUI(FirebaseUser user) {
+
+    }
+
+    private void registerNewUser(String newEmail, String newPassword) {
+        // reads email and password fields filled by new user.
+        // trims for space after entered email - happens if autofilled sometimes
+        newEmail = Email.getText().toString().trim();
+        newPassword = Password.getText().toString().trim();
+        
+        if(TextUtils.isEmpty(newEmail)) {
+            Toast.makeText(loginActivity, ""+ R.string.emailEmpty, Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(newPassword)) {
+            Toast.makeText(loginActivity, ""+ R.string.passEmpty, Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (newPassword.length() < 8) {
+            Toast.makeText(loginActivity, ""+ R.string.passShort, Toast.LENGTH_SHORT).show();
+        }
+
+        mAuth.createUserWithEmailAndPassword(newEmail, newPassword).addOnCompleteListener((Activity)getContext(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "createUserWithEmail:success" + task.isSuccessful());
+                        Toast.makeText(getContext(),""+ R.string.registrationSuccess, Toast.LENGTH_SHORT).show();
+
+                        if (!task.isSuccessful()) {
+                            // Sign up failed
+                            Toast.makeText(getContext(),""+ R.string.registrationFailed, Toast.LENGTH_SHORT).show();
+                            task.getException();
+
+                        }
+                        else {
+                            // TODO send til MainActivity her - hør Mike om intent eller hvordan vi passer
+                        }
+                    }
+                });
+
+
+    }
+
+}
+
+
